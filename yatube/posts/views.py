1728d4fb6345b58a46a7eda7dd6c688cc3c519f9
Станислав Андрющенко <StAndryushchenko@yandex.ru>
2022-09-09 01:31:09 +0300

    Проблема с запуском сервера

---
+++ yatube/posts/views.py
@@ -0,0 +1,12 @@
+from curses.ascii import HT
+from email.policy import HTTP
+from http.client import HTTPResponse
+# from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+#def index(request):
+#    return HttpResponse('Главная страница')
+
+#def group_posts(request):
+#    return HttpResponse('Тут будут посты, отфильтрованные по группам')
